5dc17c1cf86c1b750437961f7c8a6e5aac08325c
Alan Monroy Bernedo <84291224+Verborrea@users.noreply.github.com>
2021-09-25 09:32:14 -0500

    Delete public directory

--- public/js/doc.js
+++
@@ -1,9 +0,0 @@
-function show_table(){
-  if(document.getElementById("side").style.display == "block"){
-    document.getElementById("side").style.background_color = "#eee";
-    document.getElementById("side").style.display = "none"; 
-  }else{
-    document.getElementById("side").style.background_color = "#ccc";
-    document.getElementById("side").style.display = "block"; 
-  }
-}
